201b45502d2d9c502fd24c36319af91645a2d5b7
chithra <chithra7123@gmail.com>
2025-09-22 15:15:31 +0100

    created the search feature

---
+++ blogs/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Blog, Category
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'category__category_name', 'author__username',
+                     'status')
+    list_display = ('id', 'title', 'category', 'status', 'is_featured')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
