0f30e7394db8f96392749810e36862cc68a181a6
chithra <chithra7123@gmail.com>
2025-10-22 13:29:25 +0100

    static media issue fixed

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+# folder where collectstatic will gather files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    'blog_main/static',
+    BASE_DIR / 'blog_main' / 'static',   # your app-level static folder
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
